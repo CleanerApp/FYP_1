48a76b9346551aaf8367628087a9adc1cdd545df
NormanLeo <17010201@myrp.edu.sg>
2019-05-02 11:36:08 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/fyp_1/MainActivity.java
@@ -0,0 +1,252 @@
+package sg.edu.rp.c346.fyp_1;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.rengwuxian.materialedittext.MaterialEditText;
+
+import sg.edu.rp.c346.fyp_1.Model.User;
+
+public class MainActivity extends AppCompatActivity {
+
+    MaterialEditText edtNewUser, edtNewPassword, edtNewEmail;
+    EditText edtUser, edtPassword;
+    TextView tvForgotPassword;
+
+    Button btnSignup, btnLogin, btnContact;
+    FirebaseDatabase database;
+    DatabaseReference users;
+
+    FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        database = FirebaseDatabase.getInstance();
+        users = database.getReference("User");
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        btnContact = findViewById(R.id.btnContact);
+
+        tvForgotPassword = findViewById(R.id.tvForgotPwd);
+
+        edtUser = findViewById(R.id.edtUsername);
+        edtPassword = findViewById(R.id.edtPassword);
+
+        btnSignup = findViewById(R.id.btnSignUp);
+        btnLogin = findViewById(R.id.btnlogin);
+
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showSignUpDialog();
+
+                }
+            });
+
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginIn(edtUser.getText().toString(), edtPassword.getText().toString());
+            }
+        });
+
+    }
+
+
+    private void loginIn(final String user, final String pwd){
+        users.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.child(user).exists()){
+                    if (!user.isEmpty()){
+                        User login = dataSnapshot.child(user).getValue(User.class);
+                        if(login.getPassword().equals(pwd)){
+                            Toast.makeText(MainActivity.this, "Login successful", Toast.LENGTH_SHORT).show();
+
+                        } else {
+                            Toast.makeText(MainActivity.this, "Wrong username/password", Toast.LENGTH_SHORT).show();
+                        }
+
+                    } else {
+                        Toast.makeText(MainActivity.this, "Please enter your username and password", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(MainActivity.this, "Username/password does not exists", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void showSignUpDialog(){
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(MainActivity.this);
+        alertDialog.setTitle("Sign Up");
+        alertDialog.setMessage("Please fill full information");
+
+        LayoutInflater inflater = this.getLayoutInflater();
+        View sign_up_layout = inflater.inflate(R.layout.sign_up_layout, null);
+
+        edtNewUser = (MaterialEditText)sign_up_layout.findViewById(R.id.edtNewUserName);
+        edtNewPassword = (MaterialEditText)sign_up_layout.findViewById(R.id.edtNewPassword);
+        edtNewEmail = (MaterialEditText)sign_up_layout.findViewById(R.id.edtNewEmail);
+
+        alertDialog.setView(sign_up_layout);
+        alertDialog.setIcon(R.drawable.ic_account_box_black_24dp);
+
+        alertDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+
+            }
+        });
+
+        alertDialog.setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+                if (validate()) {
+                    String user_email = edtNewEmail.getText().toString().trim();
+                    String user_password = edtNewPassword.getText().toString().trim();
+
+                    firebaseAuth.createUserWithEmailAndPassword(user_email, user_password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+
+                            if (task.isSuccessful()){
+                                Toast.makeText(MainActivity.this, "Registration Successful", Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(MainActivity.this, "Registration Failed", Toast.LENGTH_SHORT).show();
+                            }
+
+                        }
+                    });
+
+
+
+
+                final User user = new User(edtNewUser.getText().toString(),
+                        edtNewPassword.getText().toString(),
+                        edtNewEmail.getText().toString());
+
+
+
+                users.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        if (dataSnapshot.child(user.getUserName()).exists())
+                            Toast.makeText(MainActivity.this, "User already exists", Toast.LENGTH_SHORT).show();
+                        else {
+                            users.child(user.getUserName())
+                                    .setValue(user);
+
+
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+
+                    dialogInterface.dismiss();
+                }
+            }
+
+
+        });
+
+        alertDialog.show();
+
+
+
+        btnContact.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent4 = new Intent(MainActivity.this, contactus.class);
+                startActivity(intent4);
+            }
+        });
+
+        tvForgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent3 = new Intent(MainActivity.this, forgotpass.class);
+                startActivity(intent3);
+            }
+        });
+
+
+
+
+
+        /*btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                    Intent intent = new Intent(MainActivity.this, login.class);
+                    startActivity(intent);
+
+                }
+            });
+
+    tvsignup.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent2 = new Intent (MainActivity.this, forgotpass.class);
+                    startActivity(intent2);
+                }
+            });
+
+
+
+*/
+        }
+
+        private Boolean validate(){
+
+            String name = edtNewUser.getText().toString();
+            String password = edtNewPassword.getText().toString();
+            String email = edtNewEmail.getText().toString();
+
+            if (name.isEmpty() || password.isEmpty() || email.isEmpty()){
+                Toast.makeText(this, "Please enter all the details", Toast.LENGTH_SHORT).show();
+            } else {
+                return true;
+            }
+
+            return false;
+        }
+
+    }
