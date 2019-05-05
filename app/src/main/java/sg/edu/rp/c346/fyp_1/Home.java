c86fe8ea2d510e65ea7b3e13a4355fd4042ae630
NormanLeo <17010201@myrp.edu.sg>
2019-05-06 00:53:44 +0800

    Commit

--- app/src/main/java/sg/edu/rp/c346/fyp_1/Home.java
+++ app/src/main/java/sg/edu/rp/c346/fyp_1/Home.java
@@ -1,7 +1,11 @@
 package sg.edu.rp.c346.fyp_1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class Home extends AppCompatActivity {
 
@@ -12,6 +16,27 @@ public class Home extends AppCompatActivity {
 
 
 
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.option_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.profile_page:
+                Intent profile = new Intent(Home.this, Profile.class);
+                startActivity(profile);
+            case R.id.contact_us:
+                Intent us = new Intent(Home.this, contactus.class);
+                startActivity(us);
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
